ce43b1321a0c4853bf2ad946494f3967d3f8c5b1
Ruby M <ranmi.chaos.fox@gmail.com>
2017-10-18 16:19:52 -0400

    Writing huffman compressed indices to file. Have to limit dictionary somehow for actual compression

--- LZWCompress.py
+++ LZWCompress.py
@@ -77,6 +77,24 @@ for i in range(0, cur_dictval):
         output.write((0).to_bytes(4, byteorder = "big"))
 
 # Write encoded indices
+towrite = 0
+bits_written = 0
+for index in indices:
+    code = huff_table[index]
+    for char in code:
+        if char == "1":
+            bit_flicker = 1 << (7-bits_written)
+            towrite = towrite | bit_flicker
+        bits_written = bits_written + 1
 
+        # when buffer is full, flush and reset
+        if bits_written == 8:
+            output.write(towrite.to_bytes(1, byteorder = "big"))
+            towrite = 0
+            bits_written = 0
+
+# Write partially-full buffer
+if not bits_written == 0:
+    output.write(towrite.to_bytes(1, byteorder = "big"))
 
 output.close()

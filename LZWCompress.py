74891126eecaca56a7598782ef19d38480001098
Ruby M <ranmi.chaos.fox@gmail.com>
2017-10-17 20:37:02 -0400

    Working on making compress use huffman, but it only works for ascii characters

--- LZWCompress.py
+++ LZWCompress.py
@@ -1,7 +1,9 @@
 # LZWCompress.py
 # Compresses an input file using the LZW algorithm
 
-text = open("dracula","rb")
+import heapq as hq
+
+text = open("banana","rb")
 dictionary = {}
 indices = []
 output = open("output","w")
@@ -10,14 +12,16 @@ cur_dictval = 0
 cur_string = ""
 next_char = ""
 
-#Construct initial dictionary w/all ASCII characters
+# Construct initial dictionary w/all ASCII characters
 for i in range(0,256):
     dictionary[chr(i)] = cur_dictval
     cur_dictval = cur_dictval + 1
 
-cur_string = text.read(1)
-next_char = text.read(1)
+# Compress to list of indices with LZW algorithm
+cur_string = bytes.decode(text.read(1))
+next_char = bytes.decode(text.read(1))
 while not next_char ==  "":
+    print(next_char)
     if (cur_string + next_char) in dictionary:
         cur_string = cur_string + next_char
     else:
@@ -25,9 +29,28 @@ while not next_char ==  "":
         dictionary[(cur_string + next_char)] = cur_dictval
         cur_dictval = cur_dictval + 1
         cur_string = next_char
-    next_char = text.read(1)
+    next_char = bytes.decode(text.read(1))
 indices.append(dictionary[cur_string])
-output.write(indices)
 
 text.close()
+
+# Build huffman tree of indices for writing to file:
+# Count frequencies of each index
+freqs = {}
+for index in indices:
+   if not index in freqs:
+       freqs[index] = 1
+   else:
+       freqs[index] = freqs[index] + 1
+
+# Build forest of nodes
+forest = []
+for node in freqs:
+    hq.heappush(forest, ((freqs[node],node)))
+
+print(forest)
+
+# Build huffman tree from minheap
+# Encode and write to file
+
 output.close()

29fdabe43e5f8a4fb122ef096177666a8dbb9ad2
Ruby M <ranmi.chaos.fox@gmail.com>
2017-12-03 14:16:17 -0500

    Realized both ll and distance code lengths are compressed with same code length alphabet

--- LZ77_deflate/deflate.py
+++ LZ77_deflate/deflate.py
@@ -6,6 +6,7 @@
 import heapq as hq
 import sys
 import huff_functions as huff
+import deflate_fns as defl
 
 # -------------------------------------------------------   
 # Function that takes care of buffer for writing individual bits to file.
@@ -321,86 +322,14 @@ ll_tree = huff.buildhufftree_full(ll_frequencies)
 # Get ordered list of code lengths to create canonical huffman code 
 ll_codelengths = huff.getcodelengths(ll_tree)
 ll_codelengths_list = huff.lengthslist(range(0, 286), ll_codelengths)
-ll_canonical = makecanonical(range(0, 286), ll_codelengths_list))
+ll_canonical = huff.makecanonical(range(0, 286), ll_codelengths_list)
 print(ll_codelengths_list)
 
 # Construct list of code length codes for canonical huffman tree for lengths/literals
-# See deflate docs for length encoding scheme
-prev = -1
-repeat_length = 0
-codelengthcodes = []
-repeat_extrabits = []
-for length in ll_codelengths_list:
+codes_plus_extrabits = defl.getcodelengthcodes(ll_codelengths_list)
+codelengthcodes = codes_plus_extrabits[0]
+repeat_extrabits = codes_plus_extrabits[1]
 
-    #print("*\nprev: " + str(prev))
-    #print("rptlen: " + str(repeat_length))
-    
-    # If the code length is a repeat, increase the repeat length
-    # If we have reached the limit of repeat size, output code for repeat section
-    if prev == length:
-        repeat_length = repeat_length + 1
-        if 1 <= prev <= 15 and repeat_length == 6:
-            # Write repeat code (16) plus code for 6 repeats (3)
-            codelengthcodes.append(16)
-            repeat_extrabits.append(3)
-            repeat_length = 0
-        elif prev == 0 and repeat_length == 138:
-            # Write long zero repeat code (18) plus code for 138 repeats (127)
-            codelengthcodes.append(18)
-            repeat_extrabits.append(127)
-            repeat_length = 0
-
-    # If we have changed code lengths, output code for last repeat section if
-    # there is one, then output code for new character
-    else:
-        if repeat_length > 2:
-            if prev == 0:
-                if 3 <= repeat_length <= 10:
-                    codelengthcodes.append(17)
-                    repeat_extrabits.append(repeat_length - 3)
-                elif 11 <= repeat_length <= 138:
-                    codelengthcodes.append(18)
-                    repeat_extrabits.append(repeat_length - 11)
-            else:
-                if 3 <= repeat_length <= 6:
-                    codelengthcodes.append(16)
-                    repeat_extrabits.append(repeat_length - 3)
-
-        if repeat_length == 1:
-            codelengthcodes.append(prev)
-        if repeat_length == 2:
-            codelengthcodes.append(prev)
-            codelengthcodes.append(prev)
-
-        repeat_length = 0
-        codelengthcodes.append(length)
-        prev = length
-
-    #print(length)
-    #print(codelengthcodes)
-
-# Account for possible unprinted last repeat
-if not repeat_length == 0:
-    if repeat_length > 2:
-        if prev == 0:
-            if 3 <= repeat_length <= 10:
-                codelengthcodes.append(17)
-                repeat_extrabits.append(repeat_length - 3)
-            elif 11 <= repeat_length <= 138:
-                codelengthcodes.append(18)
-                repeat_extrabits.append(repeat_length - 11)
-        else:
-            if 3 <= repeat_length <= 6:
-                codelengthcodes.append(16)
-                repeat_extrabits.append(repeat_length - 3)
-
-    if repeat_length == 1:
-        codelengthcodes.append(prev)
-    if repeat_length == 2:
-        codelengthcodes.append(prev)
-        codelengthcodes.append(prev)
-
-print(codelengthcodes)
 
 # Compress THOSE code length codes with ANOTHER canonical huffman code and output
 # First collect frequencies

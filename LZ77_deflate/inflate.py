31d0c3cfd8af1d6067d0f67e48d4453974d4702a
Ruby M <ranmi.chaos.fox@gmail.com>
2017-12-12 11:52:40 -0500

    Adding decompressor file whoops

---
+++ LZ77_deflate/inflate.py
@@ -0,0 +1,77 @@
+# INFLATE.PY
+# Decompresses files compressed with deflate_not3
+# Theoretically.
+
+import heapq as hq
+import sys
+import huff_functions as huff
+import deflate_fns as defl
+
+# -------------------------------------------------------------
+# Function that takes care of buffer for reading individual bits from file
+cur_byte = 0
+bits_read = 0
+
+# Returns the next n bits as an integer
+def readbits(n):
+    global cur_byte
+    global bits_read
+    
+    read = 0;
+    for i in range(0, n):
+        bit_getter = 1 << (7 - bits_read)
+        bit = bit_getter & cur_byte
+        if bit != 0:
+            bit = 1
+            
+        read = read * 2
+        read = read + bit
+        bits_read = bits_read + 1
+
+        if bits_read == 8:
+            bits_read = 0
+            cur_byte = int.from_bytes(text.read(1), byteorder = "big")
+
+    return read
+
+# -----------------------------------------------------------
+
+search_capacity = 32000
+search_size = 0
+
+lookahead_capacity = 258
+lookahead_size = 0
+
+chars_sent = 0 # Position of next character to send, relative to the start of the file. (Gives a consistent frame of reference for offsets.)
+
+# Read arguments from command line to determine which file to decompress and where to 
+if len(sys.argv) == 3:
+    inputname = sys.argv[1]
+    outputname = sys.argv[2]
+elif len(sys.argv) == 2:
+    inputname = sys.argv[1]
+    outputname = sys.argv[1] + "_deflated"
+else:
+    print("Please provide at least one argument")
+    sys.exit()
+
+# Setup for lookahead and search buffers, and the dictionary "search" (which contains the locations of all the three-length strings encountered)
+text = open(inputname, "rb")
+cur_byte = int.from_bytes(text.read(1), byteorder = "big")
+search_buffer = bytearray(search_capacity)
+lookahead = bytearray(lookahead_capacity)
+search = {}
+    
+# First read in btype (currently we are only sending one block & it is dynamically compressed, so it will always be a 3-bit 6)
+btype = readbits(3)
+print(btype)
+
+clc_codelengths = {}
+
+# Read in code lengths for clc tree, which are printed in this weird order
+# .. Dangit they should each be written in 3 bits, not variable #
+for i in [16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15]:
+    clc_codelengths[i] = readbits(3)
+
+print(clc_codelengths)
+
